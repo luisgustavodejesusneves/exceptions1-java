9e7d1eaef23b9bac11ea8c2b8c9c6a69647399c0
Luis Gustavo <luisgustavo.email10@gmail.com>
2023-12-26 14:33:39 -0300

    solucao media

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -42,9 +42,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // O TimeUnit.DAYS.convert transforma os milesegundos em dias.
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		System.out.println(now);
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
